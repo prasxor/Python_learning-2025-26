33c35d7197802759e2b2f9fd4673b0f989355014
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2025-12-10 09:39:34 +0530

    Add string manipulation examples in Day-02.py
    Introduces Day-02.py with various Python string operations, including indexing, slicing, formatting, escape characters, and string methods. Useful for demonstrating basic string handling and manipulation techniques.

---
+++ Day-02.py
@@ -0,0 +1,109 @@
+# strings ( strings are arrays )
+a = "hello world"
+print(a[1])
+
+# string looping 
+for x in a:
+    print(x)
+    
+# length in string
+print(len(a))
+
+#present in string
+txt = "my name is prashant kumar sinha"
+print("prashant" in txt) #true
+
+# if statement with present 
+if "prashant" in txt:
+    print("yes, 'prashant' is present in txt")
+    
+#check if not
+print('rahul' not in txt) #true
+
+#if statement with if not
+if "rahul" not in txt:
+    print("yes, 'rahul' is not present in txt")
+    
+# python slicing strings
+str = 'prashant'
+# print(str[1:4])
+# print(str[-5:-1]) [greater value : smallest value]
+
+# python - modify strings 
+# upper
+ex = 'hello, world'
+print(ex.upper())
+# lower
+ex1 = 'HELLO WORLD'
+print(ex1.lower())
+# strip
+ex2 = " hello world! "
+print(ex2.strip()) #removes spaces from front and end
+# replace
+print(ex.replace("hello", "bye"))
+# split 
+print(ex.split(",")) #make separation using comma in list
+
+#string concatenation
+a = "hello"
+b = "world"
+c = a+ " " +b
+print(c)
+
+# string format 
+age = 22
+name = "prashant"
+txt = f"my name is {name} and my age is {age}"
+print(txt)
+
+#:.2f format
+price = 200
+txt1 = f"I sell my product at {price:.2f} and get {1+1}" # float of .00
+print(txt1)
+
+# escape characters 
+txt2 = "we are so-called \"vikings\" from the north"
+print(txt2)
+
+#\' single quote
+esc = 'hello world \'bye\''
+print(esc)
+# \\ backslash
+esc1= 'hello world \\ hello' #to use single backslash we need to type two times 
+print(esc1)
+# \n new line 
+esc2 = 'hello \nworld'
+print(esc2)
+print("/////////////////////")
+#\r carraige return
+esc3 = "Hello\rWorld"
+print(esc3)
+#\t
+esc4 = "bring\tworld"
+print(esc4)
+
+#strings methods [ all string methods return new values. they do not change the original string]
+print('//////////string methods//////////')
+#capitalize() - converts the first character to upper case
+txt = "hellO World"
+print(txt.capitalize())
+#casefold() - converts string into lower case
+print(txt.casefold())
+#center - returns a centered string
+print(txt.center(20,"/"))
+#"@".join()
+myTuple = ('apple', 'ball', 'cat')
+xjoin = "#".join(myTuple) #=apple#ball#cat
+print(xjoin)
+# count()
+txtcount = " I love cars and codes and cars are best."
+xcount = txtcount.count("cars") #count in the sentences for content how many times its repeated
+print(xcount)
+#endswith(value,startindex,endindex)
+x_endswith = txtcount.endswith('.') #true
+x_endswith_2 = txtcount.endswith('.', 0,20) # false ( index 0 to 20)
+print(x_endswith)
+print(x_endswith_2)
+#find()
+x_find = txtcount.find('codes')
+print(x_find)
