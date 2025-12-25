7a9e88aeda6cd73404b4fe4613691fa9313c15d9
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2025-12-25 11:23:59 +0530

    Create Testingboolendatatype.py

---
+++ Testingboolendatatype.py
@@ -0,0 +1,37 @@
+# a = True + True
+# print(a)
+# b = False + False
+# print(b)
+# c = True + False
+# print(c)
+# d = 1 + True
+# print(d)
+# e = True
+# print(e)
+
+# f = 1
+# print(f)
+
+# typecasting 
+
+# g = int(3.4)
+# print(g)
+# h = float(0)
+# print(h)
+
+# print(bool())
+
+# str1 = ""
+# str2 = "a"
+# print(bool(str1))
+# print(bool(str2))
+
+# oo = {}
+# print(type(oo))
+
+# oq = set()
+# print(type(oq))
+
+aa = None
+# print(type(aa))
+print(bool(aa))
