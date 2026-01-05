293ea570870d56b6476e9ec1653e64ef0de8e717
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-01-05 15:39:10 +0530

    Update main.py

--- Projects-Practices/Multi-expense-tracker/main.py
+++ Projects-Practices/Multi-expense-tracker/main.py
@@ -8,20 +8,25 @@ while True:
         ''')
 
     userChoice = str(input("Enter your choice: "))
+    # while True:
+    if userChoice.lower() == "add":
+        userInput = str(input("Enter your Expense, press 'exit' to stop: "))
+        if userInput.lower() == 'exit':
+            break
+            # continue
+    elif userChoice.lower() == "show":
+        if expenses_list[-1] == 'exit':
+            expenses_list.pop()
+            print("your expenses are : ", expenses_list)
+        else:
+            print("Your expenses are : ", expenses_list)
+        # break
+    else:
+        print("App as been exited")
+        break
+            # break
 
-    while True:
-            while True:
-                if userChoice.lower() == "add":
-                    userInput = str(input("Enter your Expense, press 'exit' to stop: "))
-                    expenses_list.append(userInput)
-                    if userInput.lower() == 'exit':
-                        break
-                elif userChoice.lower() == "show":
-                    print("Your expenses are : ", expenses_list)
-                else:
-                    print("app as been exited")
-    
-    
+
     
 #     if userChoice == 1:
 #     while True:
