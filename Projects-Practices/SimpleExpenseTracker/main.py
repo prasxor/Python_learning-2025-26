27fe93959fd53007bd8ee9a277083d5fd5c51158
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-01-05 12:19:25 +0530

    added basic expense tracker

---
+++ Projects-Practices/SimpleExpenseTracker/main.py
@@ -0,0 +1,10 @@
+# amount, category and note 
+
+amount = float(input("Enter your amount: "))
+category = input("Enter your category: ")
+note = input("Enter your note: ")
+
+print("Expenses added :")
+print("amount :", amount)
+print("category :", category)
+print("note :", note)
