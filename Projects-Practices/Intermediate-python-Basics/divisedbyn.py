28a4c256f09385a0ece7b4e62c199af2198941f4
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-03-07 18:43:42 +0530

    Update divisedbyn.py

--- Projects-Practices/Intermediate-python-Basics/divisedbyn.py
+++ Projects-Practices/Intermediate-python-Basics/divisedbyn.py
@@ -3,15 +3,15 @@
 # 		•	Print Buzz for multiples of 5
 # 		•	Print FizzBuzz for multiples of both
 
-i = 1
-while i<=100:
-    if (i%3==0 and i%5 == 0):
-        print(i, "FizzBuzz")
-    elif i%3==0:
-        print(i, "Fizz")
-    elif i%5 == 0:
-        print(i, "Buzz")
-    else:
-        print(i)
-    i+=1
+# i = 1
+# while i<=100:
+#     if (i%3==0 and i%5 == 0):
+#         print(i, "FizzBuzz")
+#     elif i%3==0:
+#         print(i, "Fizz")
+#     elif i%5 == 0:
+#         print(i, "Buzz")
+#     else:
+#         print(i)
+#     i+=1
     
