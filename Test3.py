70832142619bd9af46d10ff1620d8c640ed0536d
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-03-09 10:40:57 +0530

    Update Test3.py

--- Test3.py
+++ Test3.py
@@ -1,29 +1,37 @@
-Listly = [1,2.3,1+2j,"hello", "world", True, None, 34, 54,2.234, 13,2.0, "gowtham"]
+# Listly = [1,2.3,1+2j,"hello", "world", True, None, 34, 54,2.234, 13,2.0, "gowtham"]
 
 
-integer = 10
-floatly = 1.4
-complexly = 1+2j
-string = "hello"
+# integer = 10
+# floatly = 1.4
+# complexly = 1+2j
+# string = "hello"
 
-def collection(myList):
-    numbers = []
-    strings = []
-    others = []
-    for item in myList:
-        if isinstance(item, (int,float,complex)) and not isinstance(item, bool):
-            numbers.append(item)
-        elif isinstance(item, str):
-            strings.append(item)
-        else:
-            others.append(item)
+# def collection(myList):
+#     numbers = []
+#     strings = []
+#     others = []
+#     for item in myList:
+#         if isinstance(item, (int,float,complex)) and not isinstance(item, bool):
+#             numbers.append(item)
+#         elif isinstance(item, str):
+#             strings.append(item)
+#         else:
+#             others.append(item)
     
-    return numbers,strings,others
+#     return numbers,strings,others
 
-numbers,strings,others = collection(Listly)
-# print(numbers,strings,others)
-print("Numbers are : ", numbers)
-print("Strings are : ", strings)
-print("Others are : ", others)
+# numbers,strings,others = collection(Listly)
+# # print(numbers,strings,others)
+# print("Numbers are : ", numbers)
+# print("Strings are : ", strings)
+# print("Others are : ", others)
 
-# print(Listly)
+# # print(Listly)
+
+# def allTotalOperation(a,b):
+    # sum = a+b
+    # minus = a-b
+    # multi = a*b
+    # div = a/b
+    # return sum,minus,multi,div
+
