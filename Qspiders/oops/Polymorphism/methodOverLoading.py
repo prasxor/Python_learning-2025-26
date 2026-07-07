117b5d092b820e63196dcce7adffd2b2a66c43bd
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-07-07 13:46:26 +0530

    add : tuple variable argument

--- Qspiders/oops/Polymorphism/methodOverLoading.py
+++ Qspiders/oops/Polymorphism/methodOverLoading.py
@@ -12,11 +12,25 @@
 
 # PARTIAL METHOD OVERLOADING WITH DEFAULT PARAMETER 
 
+# class Sample:
+#     def add(self,a=0, b=0,c=0):
+#         print(a+b+c)
+    
+# s1 = Sample()
+# s1.add(1,2,3)
+# s1.add(1,2)
+# s1.add(1)
+
+# TUPLE VARIABLE ARGUMENT
 class Sample:
-    def add(self,a=0, b=0,c=0):
-        print(a+b+c)
+    def add(self, *args):
+        total = 0
+        for i in args:
+            total +=i
+
+        return total
     
 s1 = Sample()
-s1.add(1,2,3)
-s1.add(1,2)
-s1.add(1)
+res = s1.add(1,2,3)
+print(res)
+print(s1.add(1,2,3,4,5,5,6,7,7,88,))
