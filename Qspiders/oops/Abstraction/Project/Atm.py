578fa9fba3dddf07594f00f0591ef42739e5c0dd
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-07-08 13:53:59 +0530

    Update Atm.py

--- Qspiders/oops/Abstraction/Project/Atm.py
+++ Qspiders/oops/Abstraction/Project/Atm.py
@@ -1,6 +1,17 @@
 from Sbi import Sbi
 
-print('''
+def customUserNameCreator(name,pin):
+    cname = set(name)
+    cname = str(customUserNameCreator)
+    pin = str(pin)
+    pin = pin[-1:-2:-1]
+    return (name + pin)
+    
+userDict = dict()
+
+
+while True:
+    print('''
     Welcome to the Sbi bank
     press option to choose
     1. create new account
@@ -9,10 +20,15 @@ print('''
     4. withdraw the amount
     5. exit
     ''')
-
-while True:
+    
     userChoice = int(input("Enter your choice from options: "))
     if userChoice == 1:
         userName = str(input("Enter your name: "))
         userPin = int(input("Enter your account pin: "))
+        balance = 0
+        Cid = customUserNameCreator(userName,userPin)
+        cid = Sbi(userName, balance)
         
+        print(f"{userName} your account as been created and your current balance is {balance}")
+    elif userChoice == 2:
+        CheckUsername = 
