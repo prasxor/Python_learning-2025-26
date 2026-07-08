2718ffd8cfa82b073117e7f56dd12687cf6d87a1
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-07-08 21:12:12 +0530

    add : check balance feature

--- Qspiders/oops/Abstraction/Project/Atm.py
+++ Qspiders/oops/Abstraction/Project/Atm.py
@@ -8,12 +8,14 @@ def customUserNameCreator(name,pin):
     return (name + pin)
     
 userDict = dict()
-
+globalBalance = 0
 
 while True:
-    print('''
+    print(f'''
     Welcome to the Sbi bank
+    ------------------------
     press option to choose
+    ------------------------
     1. create new account
     2. check existed balance
     3. deposit the amount
@@ -24,13 +26,36 @@ while True:
     userChoice = int(input("Enter your choice from options: "))
     if userChoice == 1:
         userName = str(input("Enter your name: "))
-        userPin = int(input("Enter your account pin: "))
-        balance = 0
-        cid = customUserNameCreator(userName,userPin)
-        cid = Sbi(userName, balance)
-        
-        print(f"{userName} your account as been created and your current balance is {balance}")
+        while True:
+            print("Create a new pin")
+            userPin = int(input("Enter your account pin: "))
+            print("Confirm your pin")
+            confirmUserPin = int(input("Enter your account pin: "))
+            if userPin == confirmUserPin:
+                balance = 0
+                balance = globalBalance
+                cid = customUserNameCreator(userName,userPin)
+                cid = Sbi(userName, balance)
+                print(f"{userName} your account as been created and your current balance is {balance}")
+                # userDict[userName] = confirmUserPin
+                userDict.setdefault(userName, confirmUserPin)
+                break
+            else:
+                print("pin is incorrect, create again")
+                break
     elif userChoice == 2:
-        CheckUsername = 0
+        checkUsername = str(input("Enter your user name : "))
+        if checkUsername in userDict:
+            print(f"Welcome {checkUsername}")
+            checkPin = int(input("Enter your pin: "))
+            if checkPin == userDict[checkUsername]:
+                print("Login successful")
+                print(f"your current balance is Rs:{globalBalance}")
+            else:
+                print("pin is incorrect, try again")
+        else:
+            print("username is not found, create new account")
+    elif userChoice == 3:
+        
     else : 
         pass
