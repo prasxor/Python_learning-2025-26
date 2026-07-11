9c13b12001f195176944a22e4ce9927f40b83191
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-07-11 13:51:09 +0530

    add : exception handling and using function to act as loop

--- Qspiders/ExpectionError/CustomException/CustomException.py
+++ Qspiders/ExpectionError/CustomException/CustomException.py
@@ -1,20 +1,28 @@
-username = 'prashant'
-password = 9876
+def sample():
+    try:
+        username = 'prashant'
+        password = 9876
 
-class PasswordError(Exception):
-    pass
+        class PasswordError(Exception):
+            pass
 
-class UsernameError(Exception):
-    pass
+        class UsernameError(Exception):
+            pass
 
-userInput = str(input("Enter your username: "))
-userPass = int(input("Enter your password: "))
+        userInput = str(input("Enter your username: "))
+        userPass = int(input("Enter your password: "))
 
-if userInput == username:
-    
-    if userPass == password:
-        print(f"Welcome back, {username}")
-    else:
-        raise PasswordError("Password is invalid")
-else:
-    raise UsernameError("Username is invalid")
+        if userInput == username:
+
+            if userPass == password:
+                print(f"Welcome back, {username}")
+            else:
+                raise PasswordError("Password is invalid")
+        else:
+            raise UsernameError("Username is invalid")
+    except:
+        print("Please enter valid details")
+        sample()
+
+
+sample()
